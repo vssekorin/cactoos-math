13aa70632b6df069ec21d1b224597b46f337ce34
VsSekorin <vssekorin@gmail.com>
2018-02-20 14:34:16 +0300

    #57 fix ctors

--- src/main/java/cactoosmath/scalar/ReduceLeft.java
+++ src/main/java/cactoosmath/scalar/ReduceLeft.java
@@ -55,8 +55,7 @@ public final class ReduceLeft<T> implements Scalar<T> {
      * @param fnc Function
      */
     public ReduceLeft(final Iterable<T> src, final BiFunc<T, T, T> fnc) {
-        this.items = src;
-        this.func = new BiFuncFunc<>(fnc);
+        this(src, new BiFuncFunc<>(fnc));
     }
 
     /**
