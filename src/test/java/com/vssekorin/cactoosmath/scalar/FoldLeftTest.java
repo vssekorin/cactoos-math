0e1a958b2c3aeccbf1f340b2489baf4d299b73bd
Vseslav Sekorin <VsSekorin@gmail.com>
2018-03-01 23:29:54 +0300

    #55 FoldLeft

---
+++ src/test/java/com/vssekorin/cactoosmath/scalar/FoldLeftTest.java
@@ -0,0 +1,57 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017-2018 Vseslav Sekorin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.vssekorin.cactoosmath.scalar;
+
+import com.vssekorin.cactoosmath.iterable.Seq;
+import org.cactoos.iterable.Limited;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+/**
+ * Test case for {@link ReduceLeft}.
+ *
+ * @author Vseslav Sekorin (vssekorin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ * @checkstyle JavadocMethodCheck (500 lines)
+ * @checkstyle MagicNumberCheck (500 lines)
+ */
+public final class FoldLeftTest {
+
+    @Test
+    public void value() throws Exception {
+        MatcherAssert.assertThat(
+            new FoldLeft<>(
+                new Limited<>(
+                    5,
+                    new Seq<>(1, a -> a + 1)
+                ),
+                fst -> snd -> fst * snd,
+                6
+            ).value(),
+            CoreMatchers.equalTo(720)
+        );
+    }
+}
