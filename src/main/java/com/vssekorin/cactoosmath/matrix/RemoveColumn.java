9294b288af15310ecddda0e4a27f35b95a179fb0
Vseslav Sekorin <VsSekorin@gmail.com>
2018-03-08 09:51:16 +0300

    #88 RemoveColumn

---
+++ src/main/java/com/vssekorin/cactoosmath/matrix/RemoveColumn.java
@@ -0,0 +1,66 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017-2018 Vseslav Sekorin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.vssekorin.cactoosmath.matrix;
+
+import com.vssekorin.cactoosmath.Matrix;
+
+/**
+ * Matrix without one column.
+ *
+ * @author Vseslav Sekorin (vssekorin@gmail.com)
+ * @version $Id$
+ * @param <T> Type of matrix
+ * @since 0.1
+ */
+public final class RemoveColumn<T> extends MatrixEnvelope<T> {
+
+    /**
+     * Ctor.
+     * @param src The source
+     * @param nmb Column number
+     */
+    public RemoveColumn(final Matrix<T> src, final Number nmb) {
+        super(() -> () -> {
+            final int rows = new RowNumber<>(src).value();
+            final int cols = new ColumnNumber<>(src).value();
+            final T[][] array = src.asArray();
+            final T[][] result = (T[][]) new Object[rows][cols - 1];
+            for (int row = 0; row < rows; ++row) {
+                System.arraycopy(
+                    array[row], 0,
+                    result[row], 0,
+                    nmb.intValue()
+                );
+            }
+            for (int row = 0; row < rows; ++row) {
+                System.arraycopy(
+                    array[row], nmb.intValue() + 1,
+                    result[row], nmb.intValue(),
+                    cols - (nmb.intValue() + 1)
+                );
+            }
+            return result;
+        });
+    }
+}
