33332e9355f3210068b0f4911a3e39e0266a6824
Vseslav Sekorin <VsSekorin@gmail.com>
2018-03-09 22:13:00 +0300

    #80 GraphEnvelope

---
+++ src/main/java/com/vssekorin/cactoosmath/graph/GraphEnvelope.java
@@ -0,0 +1,61 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017-2018 Vseslav Sekorin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.vssekorin.cactoosmath.graph;
+
+import com.vssekorin.cactoosmath.Graph;
+import java.util.List;
+import java.util.Map;
+import org.cactoos.Scalar;
+import org.cactoos.scalar.UncheckedScalar;
+
+/**
+ * Matrix envelope.
+ *
+ * @author Vseslav Sekorin (vssekorin@gmail.com)
+ * @version $Id$
+ * @param <T> Type of graph
+ * @since 0.1
+ * @checkstyle AbstractClassNameCheck (500 lines)
+ */
+@SuppressWarnings("PMD.AbstractNaming")
+public abstract class GraphEnvelope<T> implements Graph<T> {
+
+    /**
+     * The graph.
+     */
+    private final UncheckedScalar<Graph<T>> origin;
+
+    /**
+     * Ctor.
+     * @param graph The source
+     */
+    public GraphEnvelope(final Scalar<Graph<T>> graph) {
+        this.origin = new UncheckedScalar<>(graph);
+    }
+
+    @Override
+    public final Map<T, List<T>> asMap() throws Exception {
+        return this.origin.value().asMap();
+    }
+}
