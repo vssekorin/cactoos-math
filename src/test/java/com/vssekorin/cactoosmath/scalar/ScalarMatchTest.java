0e4e069817cfbad234e47a61f79361c63e23d9fc
Vseslav Sekorin <VsSekorin@gmail.com>
2018-05-02 02:24:08 +0300

    fix old scalar 2

--- src/test/java/com/vssekorin/cactoosmath/scalar/ScalarMatchTest.java
+++ src/test/java/com/vssekorin/cactoosmath/scalar/ScalarMatchTest.java
@@ -53,7 +53,7 @@ public final class ScalarMatchTest {
                 ),
                 () -> 0
             ),
-            Matchers.equalTo(8)
+            Matchers.equalTo(1)
         );
     }
 }
